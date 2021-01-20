8bd87f5d3f7e38673a9144380ffe391f25d84a3c
csk <cskcvarma13@gmail.com>
2021-01-20 17:50:56 -0500

    version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'simple-calculator';
+  constructor() {
+    this.calculatorForm.get('kcsYouOwn').valueChanges.subscribe(v => {
+     this.dailyResult = this.calculateAndUpdate(v);
+    });
+    this.calculatorForm.get('dailyTradingVolume').valueChanges.subscribe(v => {
+      this.calculatorForm.get('tradingFeesCollected').patchValue(
+        (this.calculatorForm.get('averageTradingFee').value / 100) * (this.calculatorForm.get('dailyTradingVolume').value / 100));
+    });
+  }
+  public dailyResult = 0.00;
+  public calculatorForm: FormGroup = new FormGroup({
+    kcsStacked: new FormControl(100000000),
+    kcsYouOwn: new FormControl(),
+    dailyTradingVolume: new FormControl(100000000),
+    averageTradingFee: new FormControl(0.1),
+    tradingFeesCollected: new FormControl()
+  });
+
+  ngOnInit(): void {
+    this.calculatorForm.get('tradingFeesCollected').patchValue(
+      (this.calculatorForm.get('averageTradingFee').value / 100) * (this.calculatorForm.get('dailyTradingVolume').value / 100));
+  }
+  calculateAndUpdate(v): number{
+    const value: any = this.calculatorForm.value;
+    return (((value.averageTradingFee / 100) * (value.dailyTradingVolume / 2)) / value.kcsStacked) * v;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
